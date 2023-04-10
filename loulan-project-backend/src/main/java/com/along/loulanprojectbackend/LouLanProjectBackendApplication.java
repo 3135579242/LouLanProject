b16f9f5385654957a53bd2eb29b8602e9f34c823
along <3135579242@qq.com>
2023-04-10 22:27:57 +0800

    创建项目

---
+++ loulan-project-backend/src/main/java/com/along/loulanprojectbackend/LouLanProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.along.loulanprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LouLanProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LouLanProjectBackendApplication.class, args);
+	}
+
+}
---
+++ loulan-project-backend/src/test/java/com/along/loulanprojectbackend/LoulanProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.along.loulanprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoulanProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
